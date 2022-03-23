d8bb38a38ff9e95aa4254cdab34d89a55523f73c
alvinist27 <suhrobhotamov496@gmail.com>
2022-03-23 19:17:44 +0300

    Главная страница вывод данных

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Post
+from django.core.paginator import Paginator
 
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
-        return render(
-            request,
-            'blog/index.html'
-        )
+        posts = Post.objects.all()
+        paginator = Paginator(posts, 6)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        return render(request, 'blog/index.html', context={'page_obj': page_obj})
