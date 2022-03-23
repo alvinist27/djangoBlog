5a539ce0dd9e3a04361df9a2fd54edfa5e4553b8
alvinist27 <suhrobhotamov496@gmail.com>
2022-03-24 00:13:59 +0300

    Добавление страницы поста

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
-from .views import MainView
+from .views import MainView, PostDetailView
 from django.urls import path
 
 urlpatterns = [
     path('', MainView.as_view(), name='index'),
+    path('blog/<slug>/', PostDetailView.as_view(), name='post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from .models import Post
 from django.core.paginator import Paginator
@@ -11,3 +11,9 @@ class MainView(View):
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
         return render(request, 'blog/index.html', context={'page_obj': page_obj})
+
+
+class PostDetailView(View):
+    def get(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, url=slug)
+        return render(request, 'blog/post.html', context={'post': post})
